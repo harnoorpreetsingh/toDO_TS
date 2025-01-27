2fc69e955d0bb1e9ae20227b752ba938f5edde0a
harnoorpreetsingh <helloharnoor@gmail.com>
2025-01-27 10:11:20 +0530

    d->D

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import TodoList from './components/TodoList'
 import { ToDo } from './types/todo'
 
 function App() {
-  // const todos =[{id:1, work:"buy milk"}]
   const [data, setData] = useState<ToDo[]>([])
   const addToDoHandler=(text:string)=>{
     const id = Math.random().toString()
@@ -14,7 +13,7 @@ function App() {
   }
 
   const removeToDo =(todoId:string)=>{
-    const newToDo = data.filter((data: Todo)=>{
+    const newToDo = data.filter((data: ToDo)=>{
       return data.id !== todoId
     })
     setData(newToDo)
